1874511b5f6bc237e4115c4dff328647730f403b
Hamid Erfani <hamid_erfani@hotmail.com>
2018-11-05 14:11:09 +0100

    Lagt till en kommentar

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-
+//Kommentar
 public class Main {
     public static void main(String[] args) {
         try {
